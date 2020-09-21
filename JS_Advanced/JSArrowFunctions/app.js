fe5743462eaeb5221c5ffbb4ccdb20bcb3ee1cde
Sebastiaan Jansen <sebastiaan_jansen3@hotmail.nl>
2020-09-21 15:03:15 +0200

    JS ArrowFunctions

---
+++ JS_Advanced/JSArrowFunctions/app.js
@@ -0,0 +1,19 @@
+// Deel A
+const ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+};
+ikRockArrowFunctions()
+// Deel B
+const fivePlusSeven = () => 5 + 7;
+console.log(fivePlusSeven());
+// Deel C
+() => 1 + 2;
+// Deel D
+const myFunction = (a, b) => a + b;
+console.log(myFunction(2, 2));
+// Deel E
+const addFive = a => a + 5;
+console.log(addFive(2));
+// Deel F
+const createObject = () => ({greeting: "hoi"})
+console.log(createObject());
