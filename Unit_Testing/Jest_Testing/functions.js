7ce79679eb89e363e4bfceef2e96a8c62877830e
Sebastiaan Jansen <sebastiaan_jansen3@hotmail.nl>
2020-10-28 08:39:52 +0100

    initial commit

---
+++ Unit_Testing/Jest_Testing/functions.js
@@ -0,0 +1,18 @@
+const functions = {
+    add: (num1, num2) => num1 + num2,
+    isNull: function() {
+        return null;
+    },
+    checkValue: function(x) {
+        return x;
+    },
+    createUser: function() {
+        const user = { 
+            firstName: 'Brad' 
+        };
+        user['lastName'] = 'Traversy';
+        return user;
+    }
+}
+
+module.exports = functions;
