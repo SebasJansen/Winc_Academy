e75c9f876b5683395b45d4622bc05c7181bd9364
Sebastiaan Jansen <sebastiaan_jansen3@hotmail.nl>
2020-10-28 18:01:27 +0100

    initial commit

---
+++ EindOpdrachten/Sebastiaan_Jansen-TDD_Groentetuin/farm.js
@@ -0,0 +1,51 @@
+// const get_yield_for_plant = (plant) => {
+//     return plant.yield;
+// }
+// const get_yield_for_crop = (cropObject) => {
+//     const yield_for_plant = get_yield_for_plant(cropObject.crop);
+//     const yield_for_crop = yield_for_plant * cropObject.num_crops;
+//     return yield_for_crop;
+// }
+// const get_total_yield = (cropsInput) => {
+//     let total_yield = 0;
+//     cropsInput.crops.forEach(crop => {
+//         total_yield += get_yield_for_crop(crop);
+//     });
+//     return total_yield;
+// }
+// const get_costs_for_plant = (plant) => {
+//     return plant.costs;
+// }
+// const get_costs_for_crop = (cropObject) => {
+//     const costs_for_plant = get_costs_for_plant(cropObject.crop);
+//     const costs_for_crop = costs_for_plant * cropObject.num_crops;
+//     return costs_for_crop;
+// }
+// const get_revenue_for_plant = (plant) => {
+//     return plant.revenue * plant.yield;
+// }
+// const get_revenue_for_crop = (cropObject) => {
+//     const revenue_for_plant = get_revenue_for_plant(cropObject.crop);
+//     const revenue_for_crop = revenue_for_plant * cropObject.num_crops;
+//     return revenue_for_crop;
+// }
+// const get_profit_for_plant = (plant) => {
+//     return get_revenue_for_plant(plant) - get_costs_for_plant(plant);
+// }
+// const get_profit_for_crop = (cropObject) => {
+//     const profit_for_plant = get_profit_for_plant(cropObject.crop);
+//     const profit_for_crop = profit_for_plant * cropObject.num_crops;
+//     return profit_for_crop;
+// }
+
+module.exports = {
+    // get_yield_for_plant,
+    // get_yield_for_crop,
+    // get_total_yield,
+    // get_costs_for_plant,
+    // get_costs_for_crop,
+    // get_revenue_for_plant,
+    // get_revenue_for_crop,
+    // get_profit_for_plant,
+    // get_profit_for_crop,
+};
---
+++ EindOpdrachten/Sebastiaan_Jansen-TDD_Groentetuin/farm.test.js
@@ -0,0 +1,137 @@
+const {
+    // get_yield_for_plant,
+    // get_yield_for_crop,
+    // get_total_yield,
+    // get_costs_for_plant,
+    // get_costs_for_crop,
+    // get_revenue_for_plant,
+    // get_revenue_for_crop,
+    // get_profit_for_plant,
+    // get_profit_for_crop,
+} = require("./farm");
+
+describe("get_yield_for_plant", () => {
+    const corn = {
+        name: "corn",
+        yield: 30,
+    };
+    test("Get yield for plant with no environment factors", () => {
+        expect(get_yield_for_plant(corn)).toBe(30);
+    });
+});
+
+describe("get_yield_for_crop", () => {
+    test("Get yield for crop, simple", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+        };
+        const input = {
+            crop: corn,
+            num_crops: 10,
+        };
+        expect(get_yield_for_crop(input)).toBe(30);
+    });
+});
+
+describe("get_total_yield", () => {
+    test("Calculate total yield with multiple crops", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+        };
+        const pumpkin = {
+            name: "pumpkin",
+            yield: 4,
+        };
+        const crops = [
+            { crop: corn, num_crops: 5 },
+            { crop: pumpkin, num_crops: 2 },
+        ];
+        expect(get_total_yield({ crops })).toBe(23);
+    });
+    test("Calculate total yield with 0 amount", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+        };
+        const crops = [{ crop: corn, num_crops: 0 }];
+        expect(get_total_yield({ crops })).toBe(0);
+    });
+});
+// describe("get_costs_for_plant", () => {
+//     const corn = {
+//         name: "corn",
+//         yield: 30,
+//         costs: 15,
+//     };
+//     test("Get cost for one seed", () => {
+//         expect(get_costs_for_plant(corn)).toBe(15);
+//     });
+// });
+// describe("get_costs_for_crop", () => {
+//     test("Get costs for crop", () => {
+//         const corn = {
+//             name: "corn",
+//             yield: 3,
+//             costs: 1,
+//         };
+//         const input = {
+//             crop: corn,
+//             num_crops: 10,
+//         };
+//         expect(get_costs_for_crop(input)).toBe(10);
+//     });
+// });
+// describe("get_revenue_for_plant", () => {
+//     const corn = {
+//         name: "corn",
+//         yield: 30,
+//         costs: 15,
+//         revenue: 5,
+//     };
+//     test("Get revenue for one plant", () => {
+//         expect(get_revenue_for_plant(corn)).toBe(150);
+//     });
+// });
+// describe("get_revenue_for_crop", () => {
+//     test("Get revenue for crop", () => {
+//         const corn = {
+//             name: "corn",
+//             yield: 3,
+//             costs: 1,
+//             revenue: 2,
+//         };
+//         const input = {
+//             crop: corn,
+//             num_crops: 10,
+//         };
+//         expect(get_revenue_for_crop(input)).toBe(60);
+//     });
+// });
+// describe("get_profit_for_plant", () => {
+//     const corn = {
+//         name: "corn",
+//         yield: 30,
+//         costs: 15,
+//         revenue: 5,
+//     };
+//     test("Get profit for one plant", () => {
+//         expect(get_profit_for_plant(corn)).toBe(135);
+//     });
+// });
+// describe("get_profit_for_crop", () => {
+//     test("Get profit for crop", () => {
+//         const corn = {
+//             name: "corn",
+//             yield: 3,
+//             costs: 1,
+//             revenue: 2,
+//         };
+//         const input = {
+//             crop: corn,
+//             num_crops: 10,
+//         };
+//         expect(get_profit_for_crop(input)).toBe(50);
+//     });
+// });
